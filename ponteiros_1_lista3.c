c5f9339554b7916e371a9a7cc11cd17122c771ce
stoled0 <sarahalmeida6501@gmail.com>
2026-09-21 19:52:12 -0300

    Add files via upload

---
+++ ponteiros_1_lista3.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 3, j = 5;
+    int *p = NULL;
+    int *q = NULL;
+    p = &i;
+    q = &j;
+    
+    printf("%p \n", &p);
+    printf("%d \n", *p - *q);
+    printf("%d \n", *p * *q);
+    return 0;
+}
